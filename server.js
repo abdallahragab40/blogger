a35b0149793ef9f1e858017406c1906e9831193b
abdallahragab40 <abdallahragab40@gmail.com>
2020-05-13 22:07:55 +0200

    Add Routes

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
+// Connect database
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
+app.use(express.json());
 
 app.get("/", (req, res) => res.send("App running"));
+
+// Routes
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/blogs", require("./routes/api/blogs"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
